64eb1820e7d4a4dbcefeff4f3a9cedeb45fab830
jbobie58 <40305862+jbobie58@users.noreply.github.com>
2024-04-16 07:40:22 -0400

    Delete lbc directory

--- lbc/remote-state-datasource.tf
+++
@@ -1,9 +0,0 @@
-# Terraform Remote State Datasource - Remote Backend AWS S3
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket         = "tf-inadev-states"
-    key            = "apps/non-prod/inadev/terraform.tfstate"
-    region         = "us-east-2"
-  }
-}
--- lbc/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Generic Variables
-aws_region       = "us-east-2"
-environment      = "dev"
-business_divsion = "inadev"
